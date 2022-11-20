d8bf40b819d89678a9b9255b7c1424fefe9c2b6d
Martin Fernandez <Electronicsmf@aol.com>
2022-11-20 10:38:20 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { NewsService } from './_services/news.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'newsapp';
+  sources: any =[];
+  articles: any =[];
+  selectedNewsChannel: string = "Top 10 Trending News!";
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver,
+    private cdr: ChangeDetectorRef,
+    private newsApi : NewsService) {}
+
+  ngOnInit() {
+    this.newsApi.initArticles().subscribe((res:any)=>{
+      console.log(res);
+      this.articles = res.articles;
+    })
+    this.newsApi.initSources().subscribe((res:any)=>{
+      console.log(res);
+      this.sources = res.sources;
+    })
+  }
+
+  ngAfterViewInit(): void {
+      this.sideNav.opened = true;
+      this.observer.observe(['(max-width:787px)'])
+      .subscribe((res) => {
+        if(res?.matches) {
+           this.sideNav.mode = "over";
+           this.sideNav.close();
+        } else {
+          this.sideNav.mode = "side";
+          this.sideNav.open();
+        }
+      });
+      this.cdr.detectChanges();
+  }
+  searchSource(source:any){
+    this.newsApi.getArticlesByID(source.id)
+    .subscribe((res:any)=>{
+      this.selectedNewsChannel = source.name
+      this.articles = res.articles;
+    })
+  }
 }
